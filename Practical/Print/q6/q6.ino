58c772c0eb03483da59074bb0b9682ac9c780285
9731068 <hh_1927@aut.ac.ir>
2023-02-28 22:37:01 +0330

    Introducing LDR sensor

---
+++ Practical/Print/q6/q6.ino
@@ -0,0 +1,10 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Houra S. Hashemi");
+  Serial.println("9731068");
+  Serial.println("----------------");
+  delay(1000);
+}
